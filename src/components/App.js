e78d49d33fa3efb000901401b3ab63498b095fad
Ekta <edankhra@gmail.com>
2023-09-19 13:59:19 -0400

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
@@ -6,6 +6,34 @@ import PetBrowser from "./PetBrowser";
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
+
+  
+  const handleTypeChange = (selectedType) => {
+    setFilters({ type: selectedType });
+  };
+
+  const handleFindPetsClick = () => {
+    let apiUrl = "http://localhost:3001/pets";
+
+    if (filters.type !== "all") {
+      apiUrl += `?type=${filters.type}`;
+    }
+
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => setPets(data));
+  };
+
+  const handleAdoptPet = (id) => {
+    const updatedPets = pets.map((pet) => {
+      if (pet.id === id) {
+        return { ...pet, isAdopted: true };
+      }
+      return pet;
+    });
+
+    setPets(updatedPets);
+  };
 
   return (
     <div className="ui container">
@@ -15,10 +43,13 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters 
+            onChangeType={handleTypeChange}
+            onFindPetsClick={handleFindPetsClick}/>
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser 
+            pets={pets} onAdoptPet={handleAdoptPet}/>
           </div>
         </div>
       </div>
--- src/components/Pet.js
+++ src/components/Pet.js
@@ -1,24 +1,35 @@
 import React from "react";
 
-function Pet() {
+function Pet({ pet, onAdoptPet }) {
+  const { id, name, type, age, weight, gender, isAdopted } = pet;
+
+  const handleAdoptClick = () => {
+    if (!isAdopted) {
+      onAdoptPet(id);
+    }
+  };
+
   return (
     <div className="card" data-testid="pet">
       <div className="content">
         <span className="header">
           {/*'♀' OR '♂' */}
-          PET NAME
+          PET NAME: {name}
         </span>
         <div className="meta">
-          <span className="date">PET TYPE</span>
+          <span className="date">PET TYPE: {type}</span>
         </div>
         <div className="description">
-          <p>Age: PET AGE</p>
-          <p>Weight: PET WEIGHT</p>
+          <p> PET AGE: {age}</p>
+          <p> PET WEIGHT : {weight}</p>
+          <p>Gender: {gender === 'male' ? '♂' : '♀'}</p>
         </div>
+        
       </div>
       <div className="extra content">
-        <button className="ui disabled button">Already adopted</button>
-        <button className="ui primary button">Adopt pet</button>
+        {isAdopted ? (
+        <button className="ui disabled button">Already adopted</button> ) : (
+        <button className="ui primary button" onClick={handleAdoptClick}>Adopt pet</button>)}
       </div>
     </div>
   );
